b485dc294fef636d6128f760260f3ebc884338a1
Lucy Richards <lucyannerichards@gmail.com>
2015-02-12 13:13:29 -0800

    first commit

---
+++ mongodb-noorahealth/metadata.rb
@@ -0,0 +1,7 @@
+name        'mongodb-noorahealth'
+description 'Cookbook for Noora Healths MongoDB. Berkshelf based install'
+maintainer  "Noora Health"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
